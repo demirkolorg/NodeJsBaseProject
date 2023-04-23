6b4ad4f38458e1f880dd4777a8626db26aeed477
trliamg <52527207+demirkolorg@users.noreply.github.com>
2023-04-23 17:04:03 +0300

    routing altyapısı kuruldu #2

--- api/config/index.js
+++ api/config/index.js
@@ -1,5 +1,6 @@
-module.exports={
-    "LOG_LEVEL":process.env.LOG_LEVEL||"debug",
-    "CONNECTION_STRING":process.env.CONNECTION_STRING||"mongodb://127.0.0.1//27017/nodejs_base_project",
-    
+module.exports = {
+    "PORT": process.env.PORT || 3000,
+    "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
+    "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb://127.0.0.1//27017/nodejs_base_project",
+
 }
